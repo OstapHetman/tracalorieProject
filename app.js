1ed5051e63479871d1be00019742b920e3aca905
OstapHetman <prickly4528@gmail.com>
2018-04-04 15:27:25 +0300

    controllers & data structure

---
+++ app.js
@@ -0,0 +1,53 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function() {
+    // Item constructor
+    const Item = function(id, name, calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+
+    // Data structure / State
+    const data = {
+        items: [
+            { id: 0, name: 'Steak Dinner', calories: 1200 },
+            { id: 1, name: 'Cookie', calories: 400 },
+            { id: 2, name: 'Eggs', calories: 100 }
+        ],
+        currentItem: null,
+        totalCalories: 0
+    }
+
+    // Public methods
+    return {
+        logData: function() {
+            return data;
+        }
+    }
+})();
+
+// UI Controller
+const UICtrl = (function() {
+
+
+    // Public methodss
+    return {
+
+    }
+})();
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl) {
+
+    // Public methods
+    return {
+        init: function() {
+            console.log('Initializing App....')
+        }
+    }
+
+})(ItemCtrl, UICtrl);
+
+App.init();
